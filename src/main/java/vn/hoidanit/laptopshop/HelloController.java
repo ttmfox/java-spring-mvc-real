dd3a1310e58505292e707d0a53fc69d46f10e63c
ttmfox <thien0972089317@gmail.com>
2025-02-13 15:53:10 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World with Nguyen Quang Thien!";
+        return "Hello World with Nguyen Quang Thien !";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page!";
     }
 
 }
