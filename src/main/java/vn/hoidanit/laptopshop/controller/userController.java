5b0dab3b4af8ba2de8e382b12e6f1955005dd3c3
ttmfox <thien0972089317@gmail.com>
2025-02-18 21:13:51 +0700

    jsp

--- src/main/java/vn/hoidanit/laptopshop/controller/userController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/userController.java
@@ -10,27 +10,32 @@ import vn.hoidanit.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// @Controller
-// public class userController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello from controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class userController {
+
     private UserService userService;
 
-   public userController(UserService userService) {
+    public userController(UserService userService) {
         this.userService = userService;
     }
 
-    
-
-    @GetMapping("")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "hellojsp";
     }
 }
+
+// @RestController
+// public class userController {
+// private UserService userService;
+
+// public userController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
