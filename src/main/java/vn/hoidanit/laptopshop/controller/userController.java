63105c06e54d36cea613c6c758c90e840f79d5d5
ttmfox <thien0972089317@gmail.com>
2025-02-19 20:42:15 +0700

    send data

--- src/main/java/vn/hoidanit/laptopshop/controller/userController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/userController.java
@@ -3,12 +3,14 @@ package vn.hoidanit.laptopshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -27,6 +29,21 @@ public class userController {
         model.addAttribute("hoidanit", "from controller with model");
         return "hellojsp";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println("run here" + hoidanit);
+
+        return "hellojsp";
+    }
+
 }
 
 // @RestController
