903a86ce18671d050af5b944ffe6d94add9055fa
ttmfox <thien0972089317@gmail.com>
2025-02-19 16:32:04 +0700

    add model

--- src/main/java/vn/hoidanit/laptopshop/controller/userController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/userController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +21,10 @@ public class userController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "from controller with model");
         return "hellojsp";
     }
 }
